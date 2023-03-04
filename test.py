e291f7b9888f634576238efae4e3fb87a9804899
Bruce Fernandes <57676725+Bruce-Fernandes@users.noreply.github.com>
2023-03-04 11:43:34 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template
-from newspaper import Article
+import newspaper
 
 app = Flask(__name__)
 
-def get_summary(url):
-    article = Article(url=url, language='en')
-    article.download()
-    article.parse()
-    article.nlp()
-    summary = article.summary
-    return summary
+@app.route("/")
+def home():
+    indiatodaypaper = newspaper.build("https://www.indiatoday.in/technology/news", memoize_articles=False, language="en")
+    articles = []
+    for article in indiatodaypaper.articles[:3]:
+        article.download()
+        article.parse()
+        article.nlp()
+        articles.append({
+            "title": article.title,
+            "url": article.url,
+            "summary": article.summary
+        })
+    return render_template("index.html", articles=articles)
 
-@app.route('/')
-def index():
-    url = "https://tech.hindustantimes.com/mobile/iphone-14-just-turned-cheaper-for-you-know-how-to-cut-price-here-check-full-list-71677750949894.html"
-    summary = get_summary(url)
-    return render_template('index.html', summary=summary)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
